7b36c5f67e48cdfc9d588f60370ed24fb8fc8ceb
Aidan Gannon <aidan.gannon@stu.ocsb.ca>
2024-10-31 11:09:51 -0400

    edit

--- main.js
+++ main.js
@@ -33,13 +33,42 @@ function user(){
 
 }
 
+document.getElementById("playbutton").addEventListener("click", want_to_play);
 
+function want_to_play() {
+    let play_yn = prompt("Would you like to play?")
+    if (play_yn == "yes") {
+        addEventListener("click", ask_name)
+    }
+    else (play_yn == "no")
+    addEventListener("click", gameover)
+}
+function ask_name() {
+    let person = prompt("Hello there what is your name?")
+if (person == ""){
+    addEventListener("click", ask_name2)
+    }
+    else (person)
+    return person
+    
+}
 
-let person = prompt("Hello there what is your name?")
-let person3 = prompt("Ok if you don't tell me your name you will have to leave. So what is your name?")
-let person2 = prompt("Umm... Hello? What is your name?")
-if (person != null) 
-
-if (person2 != null) 
-if (person3 != null) 
-display("Game Over")
+function ask_name2() {
+    let person2 = prompt("Umm... Hello? What is your name?")
+if (person2 == ""){
+    addEventListener("click", ask_name3)
+    }
+    else (person2)
+    return person2
+}
+function ask_name3() {
+    let person3 = prompt("Ok if you don't tell me your name you will have to leave. So what is your name?")
+    if (person3 == ""){
+        addEventListener("click", gameover)
+    }
+    else (person3)
+    return person3
+}
+function gameover() {
+    window.location.href='game_over.jpg'
+}
